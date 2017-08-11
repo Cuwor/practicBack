32d3d3415fa84265f924eb985230680ef6548229
Cuwor <temichit@yandex.ru>
2017-08-11 12:59:37 +0700

    Формат данных и Связывание данных и шаблонов

--- app.js
+++ app.js
@@ -3,55 +3,106 @@ var app = express();
 
 app.set('view engine', 'ejs')
 app.get('/', function(res, res) {
-  res.render('index');
+  res.render('index', {
+    title: 'Все статьи',
+    author: 'Семен',
+    date: '15.15.2015'
+  });
 })
 app.listen(3000, function() {
   console.log('Example 3000 port');
 })
 
-app.get('/register', function (req, res) {
-  res.render('register');
+app.get('/register', function(req, res) {
+  res.render('register', {
+    title: 'Регистрация',
+    error: 'Проверьте правильность заполнения',
+    "visibleError":false
+  });
 });
 
-app.post('/register', function (req, res) {
-  res.render('register');
+app.post('/register', function(req, res) {
+  res.render('register', {
+    title: 'Регистрация',
+    error: 'Проверьте правильность заполнения',
+    "visibleError":false
+  });
 });
 
-app.post('/auth', function (req, res) {
-  res.render('auth');
+app.post('/auth', function(req, res) {
+  res.render('auth', {
+    title: 'Авторизация',
+    error: 'Неверный логин или пароль',
+    "visibleError":false
+  });
 });
-app.get('/auth', function (req, res) {
-  res.render('auth');
-});
-
-app.get('/page/:page', function (req, res) {
-  res.render('index');
-});
-
-app.get('/post/:id', function (req, res) {
-  res.render('post');
-});
-
-app.post('/post/:id', function (req, res) {
-  res.render('post');
+app.get('/auth', function(req, res) {
+  res.render('auth', {
+    title: 'Авторизация',
+    error: 'Неверный логин или пароль',
+    "visibleError":false
+  });
 });
 
-app.get('/post/new', function (req, res) {
-  res.render('new');
+app.get('/page/:page', function(req, res) {
+  res.render('index', {
+    title: 'Все статьи',
+    author: 'Семен',
+    date: '15.15.2015'
+  });
 });
 
-app.get('/post/:id/edit', function (req, res) {
-  res.render('edit');
+app.get('/post/:id', function(req, res) {
+  res.render('post', {
+    title: 'Статья',
+    author: 'Семен',
+    date: '15.15.2015'
+  });
 });
 
-app.put('/post/:id', function (req, res) {
-  res.render('post');
+app.post('/post/:id', function(req, res) {
+  res.render('post', {
+    title: 'Статья',
+    author: 'Семен',
+    date: '15.15.2015'
+  });
 });
 
-app.post('/post/new', function (req, res) {
-  res.render('new');
+app.get('/post/new', function(req, res) {
+  res.render('new', {
+    title: 'Новая статья',
+    error: 'Проверьте правильность заполнения',
+    "visibleError":false
+  });
 });
 
-app.get('/author/:id', function (req, res) {
-  res.render('author');
+app.get('/post/:id/edit', function(req, res) {
+  res.render('edit', {
+    title: 'Редактировать статью',
+    error: 'Проверьте правильность заполнения',
+    "visibleError":false
+  });
+});
+
+app.put('/post/:id', function(req, res) {
+  res.render('post', {
+    title: 'Cтатья'
+  });
+});
+
+app.post('/post/new', function(req, res) {
+  res.render('new', {
+    title: 'Новая статья',
+    error: 'Проверьте правильность заполнения',
+    "visibleError":false
+  });
+});
+
+app.get('/author/:id', function(req, res) {
+  res.render('author', {
+    title: 'Карточка автора',
+    author: 'Семен',
+    posts: [{'id':1, 'postName':'пост 1'},
+    {'id':2, 'postName':'пост 2'}]
+  });
 });
